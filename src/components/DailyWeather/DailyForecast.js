e9d6fe4ef181900d77d5bb56a275354be1e82cc1
turczerb <turczerb@gmail.com>
2025-03-03 11:31:52 +0100

    data fetch works

--- src/components/DailyWeather/DailyForecast.js
+++ src/components/DailyWeather/DailyForecast.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 import UseWeather from "../Hooks/UseWeather"; // this is my custom hook.
 
 const DailyForecast = () => {
-  const [city, setCity] = useState("London"); // Kezdeti város
+  const [city, setCity] = useState("Budapest"); // Kezdeti város
 
   // URLs generálása a város alapján
   const [urls, setUrls] = useState([]);
 
   useEffect(() => {
     const newUrls = [
-      `https://cors-anywhere.herokuapp.com/https://api.weatherapi.com/v1/current.json?key=09552ecbeb184118aed172805251702&q=${city}&aqi=no`,
-      `https://cors-anywhere.herokuapp.com/https://api.weatherapi.com/v1/forecast.json?key=09552ecbeb184118aed172805251702&q=${city}&days=2&aqi=no&alerts=no`,
-      `https://cors-anywhere.herokuapp.com/https://api.weatherapi.com/v1/alerts.json?key=09552ecbeb184118aed172805251702&q=${city}&aqi=no`,
-      `https://cors-anywhere.herokuapp.com/https://api.weatherapi.com/v1/astronomy.json?key=09552ecbeb184118aed172805251702&q=${city}&aqi=no`,
+      `https://api.weatherapi.com/v1/current.json?key=09552ecbeb184118aed172805251702&q=${city}&aqi=no`,
+      `https://api.weatherapi.com/v1/forecast.json?key=09552ecbeb184118aed172805251702&q=${city}&days=2&aqi=no&alerts=no`,
+      `https://api.weatherapi.com/v1/alerts.json?key=09552ecbeb184118aed172805251702&q=${city}&aqi=no`,
+      `https://api.weatherapi.com/v1/astronomy.json?key=09552ecbeb184118aed172805251702&q=${city}&aqi=no`,
     ];
     setUrls(newUrls);
   }, [city]); // Figyeljük a 'city' változását, nem a teljes URLs tömböt
